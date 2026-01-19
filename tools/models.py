f4754171267b4156812589efcae82be317bdf92d
sajay <sajaykannan10@gmail.com>
2026-01-19 09:39:18 +0530

    add changes

---
+++ tools/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class AiTool(models.Model):
+
+    name=models.CharField(max_length=200,unique=True)
+
+    company=models.CharField(max_length=200)
+
+    purpose=models.TextField()
+
+    current_version=models.CharField(max_length=200)
+
+    CATEGORY_OPTIONS=(
+        ("chat","chat"),
+        ("ai search","ai search"),
+        ("image generation","image generation"),
+        ("vedio generation","vedio generation"),
+        ("coding assistant","coding assistant"),
+        ("document","document"),
+        ("multi model","multi model"),
+        ("other","other")
+    )
+
+    category=models.CharField(max_length=200,choices=CATEGORY_OPTIONS,default="chat")
+
+    
+
+
